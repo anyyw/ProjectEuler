6e79ee297bf6b0def1cbb89927daf7f84c3c566e
Apatheticdinosaur <silverkni@gmail.com>
2015-04-14 11:34:45 -0700

    Appending L to the end makes java reconize long ints

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,10 @@
 public class Main {
     public static void main(String [] args) {
         LargestPalindromeProduct l = new LargestPalindromeProduct();
-        LargestPrimeFactor lpf = new LargestPrimeFactor(600851475143);
+        LargestPrimeFactor lpf = new LargestPrimeFactor(600851475143L);
         System.out.println(l.isPalindrome("9009"));
         System.out.println(l.calculate());
+        System.out.println(lpf.calculate());
 
     }
 }
